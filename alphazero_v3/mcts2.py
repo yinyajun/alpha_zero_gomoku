170341bfc16796524acd31aabcf21e68f81c2e30
yinyajun <skyblueice234@gmail.com>
2025-10-23 21:20:13 +0800

    feat: init

--- alphazero_v3/mcts2.py
+++ alphazero_v3/mcts2.py
@@ -3,7 +3,7 @@ import numpy as np
 import torch
 from typing import Optional
 
-from game import Game, Nobody
+from game import Game, Nobody, Move
 
 
 class TreeNode:
@@ -22,22 +22,22 @@ class TreeNode:
         return game.is_end is True
 
     def is_fully_expanded(self):
-        return len(self.children) > 0
+        return len(self.children) > 0  # 采用的是“全展开”，有孩子就表示已展开
 
-    def select(self, c_puct: float) -> "TreeNode":
+    def select(self, c_puct: float) -> tuple[Move, "TreeNode"]:
         """
         PUCT: Q + c * P * sqrt(N) / (1+n)
         """
+        assert self.children
         parent_sqrt = math.sqrt(self.N + 1e-8)
 
-        def puct(ch: "TreeNode") -> float:
-            P = self.P  # P(s,a)
-            Q = ch.W / ch.N if ch.N > 0 else 0.0  # Q(s,a)
-            exploit = - Q  # Q_child(parent视角) = - Q_child(child视角)
-            explore = P * parent_sqrt / (1.0 + ch.N)
+        def puct(item) -> float:
+            move, ch = item
+            exploit = - ch.Q  # Q_child(parent视角) = - Q_child(child视角)
+            explore = ch.P * parent_sqrt / (1.0 + ch.N)
             return exploit + c_puct * explore
 
-        return max(self.children.values(), key=puct)
+        return max(self.children.items(), key=puct)
 
     def evaluate(self, game: Game, pv_fn):
         if self.is_terminal(game):
@@ -57,8 +57,6 @@ class TreeNode:
 
         for move in game.available_moves():
             p = priors[move[0] * Game.size + move[1]]
-            game = game.clone()
-            game.step(move)
             self.children[move] = TreeNode(prior=p, parent=self)
 
     def backprop(self, v: float):
@@ -72,14 +70,16 @@ class TreeNode:
 
     def play_out(self, game: Game, pv_fn, c_puct: float):
         node = self
+        game = game.clone()
 
         # 1) select
-        while not node.is_terminal() and node.is_fully_expanded():  # 已经完全展开，并且没有终局，select最佳child
-            node = node.select(c_puct)
+        while not node.is_terminal(game) and node.is_fully_expanded():  # 已经完全展开，并且没有终局，select最佳child
+            move, node = node.select(c_puct)
+            game.step(move)
         # is_terminal or not expanded
 
         # 2) roll
-        priors, value = self.evaluate(game, pv_fn)
+        priors, value = node.evaluate(game, pv_fn)
 
         # 3) expand all
         if not node.is_terminal(game):
